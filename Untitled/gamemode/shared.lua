9c454c446d1ee19afe63967527632642c8de41f7
Author <epicauthorjames@gmail.com>
2015-04-06 01:11:00 +0200

    Fixed

--- Untitled/gamemode/shared.lua
+++ Untitled/gamemode/shared.lua
@@ -1,6 +1,6 @@
-GM.Name = ""
-GM.Author = ""
-GM.Email = ""
-GM.Website = ""
+GM.Name = "Untitled"
+GM.Author = "Facepunch"
+GM.Email = "newell@valve.com"
+GM.Website = "http://facepunch.com/"
 
-DeriveGamemode( "sandbox" )
+DeriveGamemode "sandbox"
